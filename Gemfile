0808cd15d79c5b7023c91f3bb47802b4f2f02035
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-04-19 14:11:47 -0500

    create customer model and factory

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   gem 'rspec-rails'
   gem 'pry'
+  gem 'factory_bot_rails'
+  gem 'faker'
 end
 
 group :development do
---
+++ spec/requests/api/v1/customer_subscription_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "Customer Subsciption API" do
+  describe 'post /api/v1/customer_subscriptions' do
+    it 'creates a customer_subscription record given a customer and a subscription' do
+      customer = create(:customer)
+      require 'pry'; binding.pry
+      subscription = create(:subscription)
+      expect(customer.subscriptions).to eq([])
+
+      headers = {'CONTENT_TYPE' => 'application/json'}
+      params = ({
+        customer: customer,
+        subscription: subscription
+      })
+
+      post "api/v1/customer_subscriptions", headers: headers, params: JSON.generate(params)
+      
+      expect(customer.subscriptions).to eq([subscription])
+      expect(subscription.customers).to eq([customer])
+
+      customer_subscription = CustomerSubscription.last
+      expect(customer_subscription.customer_id).to eq(customer.id)
+      expect(customer_subscription.subscription_id).to eq(subscription.id)
+    end
+  end
+end
